2ac95ea95c1ca69c1cb99771db2b773b7431859e
{Nghia} <{truongnghia1103@gmail.com}>
2021-07-26 15:21:00 +0700

    Add project files.

---
+++ BT1/Source.cpp
@@ -0,0 +1,104 @@
+﻿//Bài tập chương Câu lệnh lặp.Hàm nhận vào một
+//số nguyên dương n và thực hiện :
+//a.S = 1 + 2 + … + n //S = 1 + 2 + 3 + 4 + 5 = 15
+//b.S = 1^2 + 2^2 + … + n^2 // S = 1^2 + 2^2 + 3^2 + 4^2 + 5^2 = 55
+//c.S = 1 + 1 / 2 + … + 1 / n
+//d.S = 1 * 2 * … * n
+//e.S = 1!+ 2!+ … + n!
+#include<iostream>
+#include<cmath>
+using namespace std;
+int sTongA(int);		//hàm khai báo;
+int sTongB(int);
+float sTongC(int n);
+int sTongD(int);
+int sTongE(int n);
+
+int main()
+{
+	int n;
+	cout << "Nhap vao 1 so nguyen duong: ";
+	cin >> n;
+	cout << "S = 1 + 2 + n la : " << sTongA(n) << endl;
+	cout << "S = 1^2 + 2^2 + n^2 la : " << sTongB(n) << endl;
+	cout << "S = 1 + 1 / 2 + 1 / n la : " << sTongC(n) << endl;
+	cout << "S = 1 * 2 * n la : " << sTongD(n) << endl;
+	cout << "S = 1!+ 2!+ n! la : " << sTongE(n) << endl;
+
+
+	system("pause");
+	return 0;
+}
+// Định nghĩa hàm	
+//a.S = 1 + 2 + … + n
+int sTongA(int n)
+{
+	int sTongA = 0;
+	while (n <= 0);
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			sTongA += i; // Stong = sTong + i;
+		}
+
+	}
+	return sTongA;
+}
+//Định nghĩa hàm
+//b.S = 1^2 + 2^2 + … + n^2
+int sTongB(int n)
+{
+	int sTongB = 0;
+	while (n <= 0);
+	{
+		for (int i = 0; i <= n; i++)
+		{
+			sTongB += pow(i, 2);//pow là số mũ;
+		}
+	}
+	return sTongB;
+}
+//Định nghĩa hàm
+////c.S = 1 + 1 / 2 + … + 1 / n
+float sTongC(int n)
+{
+	int i;
+	float sTongC = 0;
+	while (n <= 0);
+	{
+		for (i = 1; i <= n; i++)
+		{
+			sTongC = sTongC + 1.0 / i;
+		}
+	}
+	return sTongC;
+}
+// Định nghĩa hàm
+// d.S = 1 * 2 * … * n
+int sTongD(int n)
+{
+	int sTongD = 1;
+	while (n <= 0);
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			sTongD = sTongD * i;
+		}
+	}
+	return sTongD;
+}
+//e.S = 1!+ 2!+ … + n!
+int sTongE(int n)
+{
+	int GT = 1;
+	int sTongE = 0;
+	while (n <= 0);
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			GT = GT * i;
+			sTongE = sTongE + GT;
+		}
+	}
+	return sTongE;
+}
---
+++ BT2/Source.cpp
@@ -0,0 +1,142 @@
+﻿//Các thao tác nhập xuất
+//a.Nhập mảng
+//b.Xuất mảng
+//2. Các thao tác kiểm tra
+//a.Mảng có phải là mảng toàn chẵn
+//b.Mảng có phải là mảng toàn số nguyên tố
+//c.Mảng có phải là mảng tăng dần
+#include<iostream>
+#include<ctime>
+using namespace std;
+#define MAX_A 100 //Khai báo biến hằng có giá trị la 100
+#define MAX_B 100
+
+void NhapMang(int a[], int &n); //Khai báo hàm
+//void NhapMangRanDum(int a[], int &n);
+void XuatMang(int a[], int n);
+bool KiemTraMangToanChan(int a[], int &n);
+bool KiemTraSNT(int a[], int n);
+int main()
+{
+	int	a[MAX_A];
+	int nA;
+	int b[MAX_B];
+	int nB;
+	cout << "Nhap mang A: " << endl;;
+	NhapMang(a, nA);
+	//NhapMangRanDum(a, nA);
+	if (KiemTraMangToanChan(a, nA) == true)
+
+	{
+		cout << "Mang toan so chan: " << endl;
+	}
+	else
+		cout << "Mang toan so khong phai la chan: " << endl;
+	//
+	/*if (KiemTraSNT(a, nA) == true)
+	{
+		cout << "Mang co toan SNT: " << endl;
+	}
+	else
+	{
+	cout << "Khong phai la SNT: " << endl;
+	}*/
+	cout << "Nhap mang B: " << endl;
+	NhapMang(b, nB);
+	//NhapMangRanDum(b, nB);
+	if (KiemTraMangToanChan(b, nB) == true)
+
+	{
+		cout << "Mang toan so chan: " << endl;
+	}
+	else
+	{
+		cout << "Mang khong phai la so chan: " << endl;
+	}
+	//
+	/*if (KiemTraSNT(b, nB) == true)
+	{
+		cout << "Mang co toan SNT: " << endl;
+	}
+	else
+	{
+		cout << "Khong phai la SNT: " << endl;
+	}*/
+	cout << "Xuat mang A: " << endl;
+	XuatMang(a, nA);
+	cout << "Xuat mang B: " << endl;
+	XuatMang(b, nB);
+
+	system("pause");
+	return 0;
+}
+void NhapMang(int a[], int &n)
+{
+	cout << "Nhap so luong phan tu: ";
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Nhap mang [" << i << "] = ";
+		cin >> a[i];
+	}
+}
+void XuatMang(int a[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//cout << "Xuat mang [" << i << "] = " << a[i] << endl;
+		if (i % 10 == 0) // khi chạy sẽ nhìn đẹp hơn
+		{
+			cout << endl;
+		}
+		cout << a[i] << "\t";
+	}
+}
+//void NhapMangRanDum(int a[], int &n) // Nhanh gọn hơn
+//{
+//	cout << "Nhap so luong phan tu: ";
+//	cin >> n;
+//	srand (time(NULL)); // srand
+//	for (int i = 0; i < n; i++)
+//	{
+//		//cout << "Nhap mang [" << i << "] = ";
+//		a[i] = rand() % 1000 + 0; // rand 
+//		//cin >> a[i];
+//	}
+//}
+//a.Mảng có phải là mảng toàn chẵn
+bool KiemTraMangToanChan(int a[], int &n)// hàm bool trả về true/false
+{
+	//srand(time(NULL)); // srand
+	bool KT = true;
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] % 2 != 0)
+		{
+			KT = false;
+			break;
+		}
+	}
+	return KT;
+}
+//b.Mảng có phải là mảng toàn số nguyên tố
+//bool KiemTraSNT(int a[], int n)// hàm bool trả về true/false
+//{
+//
+//	if (n < 2)
+//	{
+//		return false;
+//	}
+//	for (int i = 2; i <= a[i] / 2; i++)
+//	{
+//		if (a[i] % 2 == 0)
+//		{
+//			return false;
+//		}
+//		else
+//		{
+//			return true;
+//		}
+//	}
+//
+//}
